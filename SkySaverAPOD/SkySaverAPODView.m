b1776c4980f183593e3d5bdce10ba34a76bbbd8f
Kris Fedorenko <kfedoren@gmail.com>
2020-07-25 14:56:19 -0400

    First successful drawing of an image

--- SkySaverAPOD/SkySaverAPODView.m
+++ SkySaverAPOD/SkySaverAPODView.m
@@ -10,14 +10,21 @@
 
 @implementation SkySaverAPODView
 
+static NSRect picRect;
+static NSRect mainRect;
+
 - (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
 {
     self = [super initWithFrame:frame isPreview:isPreview];
     if (self) {
         [self setAnimationTimeInterval:1/30.0];
         
-        NSRect mainRect = CGRectMake(0, 0, frame.size.width, frame.size.height);
-        [self drawRect:mainRect];
+        mainRect = CGRectMake(0, 0, frame.size.width, frame.size.height);
+        
+        
+        picRect = CGRectMake(0, 0, 500, 500);
+
+        
         
     }
     return self;
@@ -38,10 +45,36 @@
     [super drawRect:rect];
     [[NSColor greenColor] setFill];
     NSRectFill(rect);
+    
+//    [[NSColor blackColor] setFill];
+//    NSRectFill(picRect);
+    [super drawRect:picRect];
+    
+//    NSURL* picURL = [NSURL fileURLWithPath:@"https://apod.nasa.gov/apod/image/2007/DSC7590-Leutenegger1200c.jpg"];
+//    NSImage* pic = [[NSImage alloc] initByReferencingURL:picURL];
+    
+
+    NSImage* pic = [[NSImage alloc] initByReferencingFile:@"/Users/Christik/Pictures/DSC7590-Leutenegger.jpg"];
+    
+    if (!pic.isValid){
+        NSLog(@"image not created\n");
+    }
+
+    [pic drawInRect:picRect];
 }
 
 - (void)animateOneFrame
 {
+    //[self drawRect:mainRect];
+    
+//    NSImage* pic = [[NSImage alloc] initByReferencingFile:@"https://apod.nasa.gov/apod/image/2007/DSC7590-Leutenegger1200c.jpg"];
+//    
+//    if (!pic.isValid){
+//        NSLog(@"image not created\n");
+//    }
+//    
+//    [pic drawInRect:picRect];
+    
     return;
 }
 
