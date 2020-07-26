6a8e1ec94d45c319642322e189453a732cf87710
Kris Fedorenko <kfedoren@gmail.com>
2020-07-26 11:33:35 -0400

    Got transparent rect to work

--- SkySaverAPOD/SkySaverAPODView.m
+++ SkySaverAPOD/SkySaverAPODView.m
@@ -32,11 +32,6 @@ static NSDictionary* APODdata;
         // Create NSURLSessionDataTask task object by url and session object.
         NSURLSessionDataTask* task = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
                 
-//            NSLog(@"%@",[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
-            
-            // Print response JSON data in the console.
-//            NSLog(@"%@", [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil]);
-            
             APODdata = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
 //            NSLog(@"HDURL: %@", APODdata[@"hdurl"]);
 
@@ -47,7 +42,6 @@ static NSDictionary* APODdata;
         [task resume];
         
         while (APODdata == nil){
-            //NSLog(@"Waiting for data...");
             // do not leave initialization until data is fetched
         }
         
@@ -70,13 +64,14 @@ static NSDictionary* APODdata;
 - (void)drawRect:(NSRect)rect
 {
     NSLog(@"already in drawRect");
+    NSLog(@"isOpaque: %hhd", [self isOpaque]);
     // background
     [super drawRect:rect];
     [[NSColor greenColor] setFill];
     NSRectFill(rect);
     
     // picture
-    if (APODdata){
+    //if (APODdata){
     //    NSImage* pic = [[NSImage alloc] initByReferencingURL:[NSURL URLWithString:@"https://apod.nasa.gov/apod/image/2007/DSC7590-Leutenegger1200c.jpg"]];
     
     NSString* hdurl = [NSString stringWithFormat:@"%@", APODdata[@"hdurl"]];
@@ -90,29 +85,35 @@ static NSDictionary* APODdata;
     if (!pic.isValid){
         NSLog(@"image not created\n");
     }
-
-    [pic drawInRect:picRect];
-    } else {
-        NSLog(@"Not loaded yet....");
-    }
     
+//    [pic drawInRect:picRect];
+    //    } else {
+    //        NSLog(@"Not loaded yet....");
+    //    }
+    //
     // text
-    NSString* desc = @"The multi-mirror, 17 meter-diameter MAGIC telescopes reflect this starry night sky from the Roque de los Muchachos European Northern Observatory on the Canary Island of La Palma. MAGIC stands for Major Atmospheric Gamma Imaging Cherenkov and the telescopes can see the brief flashes of optical light produced in particle air showers as high-energy gamma rays impact the Earth's upper atmosphere. On July 20, two of the three telescopes in view were looking for gamma rays from the center of our Milky Way galaxy. In reflection they show the bright stars of Sagittarius and Scorpius near the galactic center to the southeast. Beyond the segmented-mirror arrays, above the northwest horizon and below the Big Dipper is Comet NEOWISE. NEOWISE stands for Near Earth Object Wide-field Infrared Survey Explorer. That's the Earth-orbiting satellite used to discover the comet designated C/2020 F3, but you knew that. ";
+    NSString* desc = [NSString stringWithFormat:@"%@", APODdata[@"explanation"]];
     
 
     NSSize textSize = NSMakeSize(rect.size.width/3, rect.size.height/4);
-    NSRect textRect = CGRectMake(rect.size.width-textSize.width,
-                                     rect.size.height-textSize.height,
-                                     textSize.width, textSize.height);
-    [super drawRect:textRect];
-    [[NSColor darkGrayColor] setFill];
-    NSRectFill(textRect);
+    NSRect textRect = {{rect.size.width-textSize.width,
+        rect.size.height-textSize.height}, {textSize.width, textSize.height}};
+//        //CGRectMake(rect.size.width-textSize.width,
+//                                     rect.size.height-textSize.height,
+//                                     textSize.width, textSize.height);
     
-    NSFont* font = [NSFont fontWithName:@"Helvetica" size:0.03*rect.size.height];
-    NSDictionary* attributes = @{ NSFontAttributeName: font,
-                                  NSForegroundColorAttributeName: [NSColor lightGrayColor]
-    };
-    [desc drawInRect:textRect withAttributes:attributes];
+    [super drawRect:textRect];
+    [pic drawInRect:picRect];
+    [[NSColor colorWithSRGBRed:0.3 green:1 blue:0.3 alpha:0.1] setFill];
+    //NSRectFill(textRect);
+
+    NSRectFillUsingOperation(textRect, NSCompositingOperationSourceOver );//NSCompositingOperationSourceOver);
+    
+//    NSFont* font = [NSFont fontWithName:@"Helvetica" size:0.03*rect.size.height];
+//    NSDictionary* attributes = @{ NSFontAttributeName: font,
+//                                  NSForegroundColorAttributeName: [NSColor lightGrayColor]
+//    };
+//    [desc drawInRect:textRect withAttributes:attributes];
     
 }
 
